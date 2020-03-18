1c6327d8064019c393951a92b987b74620df87f3
George Miller <git@duffleman.co.uk>
2020-03-18 16:46:17 +0000

    add waitgroups, err channels, and handle things better

--- app/resave_hashes.go
+++ app/resave_hashes.go
@@ -2,6 +2,7 @@ package app
 
 import (
 	"context"
+	"sync"
 
 	"dflimg"
 )
@@ -11,29 +12,58 @@ func (a *App) ResaveHashes(ctx context.Context) error {
 	if err != nil {
 		return err
 	}
-
+	errCh := make(chan error, 1)
 	c := make(chan *dflimg.ShortFormResource)
+	wg := &sync.WaitGroup{}
 
-	// start 3 workers
-	go a.doDaWork(ctx, c)
-	go a.doDaWork(ctx, c)
-	go a.doDaWork(ctx, c)
+	// I moved this above to stop blocking execution.
+	// as we're now looking for an empty channel (Exhibit A) it would have been
+	// empty immediately had it happened after the workers
+	go func() {
+		for _, r := range resources {
+			c <- r
+		}
+		close(c)
+	}()
 
-	// dump all resources into the channel
-	for _, r := range resources {
-		c <- r
+	for i := 0; i < 3; i++ {
+		wg.Add(1)
+		go a.doDaWork(ctx, wg, c, errCh)
 	}
+
+	if len(errCh) > 0 {
+		err := <-errCh
+		return err
+	}
+
+	wg.Wait()
 
 	return nil
 }
 
-func (a *App) doDaWork(ctx context.Context, ch chan *dflimg.ShortFormResource) {
+func (a *App) doDaWork(ctx context.Context, wg *sync.WaitGroup, ch chan *dflimg.ShortFormResource, errCh chan error) {
+	defer wg.Done()
+
 	for {
 		select {
-		case r := <-ch:
-			hash := a.makeHash(r.Serial)
+		case r, ok := <-ch:
+			if !ok {
+				// Exhibit A: the channel is now closed and empty
+				return
+			}
 
-			a.db.SaveHash(ctx, r.Serial, hash)
+			hash := a.makeHash(r.Serial)
+			// if SaveHash returns an error, whack it on the channel
+			err := a.db.SaveHash(ctx, r.Serial, hash)
+			if err != nil {
+				errCh <- err
+				return
+			}
+
+		case <-ctx.Done():
+			// ctx is cancelled, throw the error on the channel
+			errCh <- ctx.Err()
+			return
 		}
 	}
 }
