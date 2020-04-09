8092cf6300f012667cbc6599280fb48ecda9275f
George Miller <git@duffleman.co.uk>
2020-04-09 23:35:56 +0100

    set_nsfw endpoint

---
+++ rpc/set_nsfw.go
@@ -0,0 +1,43 @@
+package rpc
+
+import (
+	"dflimg"
+	"dflimg/dflerr"
+	"dflimg/rpc/middleware"
+	"encoding/json"
+	"net/http"
+)
+
+func (r *RPC) SetNSFW(w http.ResponseWriter, req *http.Request) {
+	ctx := req.Context()
+
+	key := ctx.Value(middleware.UsernameKey)
+	if key == nil || key == "" {
+		r.handleError(w, req, dflerr.New(dflerr.AccessDenied, dflerr.M{"username": key}))
+		return
+	}
+	username := ctx.Value(middleware.UsernameKey).(string)
+
+	body := &dflimg.SetNSFWRequest{}
+	err := json.NewDecoder(req.Body).Decode(body)
+	if err != nil {
+		r.handleError(w, req, err)
+		return
+	}
+
+	resource, err := r.app.GetResource(ctx, body.Query)
+	if err != nil {
+		r.handleError(w, req, err)
+		return
+	}
+
+	if resource.Owner != username {
+		r.handleError(w, req, dflerr.New(dflerr.AccessDenied, nil))
+		return
+	}
+
+	err = r.app.SetNSFW(ctx, resource.ID, body.NSFW)
+	r.handleError(w, req, err)
+
+	return
+}
